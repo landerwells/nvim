8e07001dc5ae2aeaec4f6e8bea5b549ca6bd01e6
Lander Wells <landerwells@gmail.com>
2023-12-25 22:36:10 -0600

    copilot binds

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
   'github/copilot.vim',
+  -- I want to have leader c toggle copilot completions
+  config = function()
+    vim.keymap.set("n", "<leader>ce", ':Copilot enable<CR>')
+    vim.keymap.set("n", "<leader>cd", ':Copilot disable<CR>')
+  end
 }
