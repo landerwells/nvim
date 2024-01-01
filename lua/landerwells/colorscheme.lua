0643a686d993e8181f07d39df53ae3896bb615ff
landerwells <landerwells@gmail.com>
2024-01-01 23:26:42 +0100

    color scheme

--- lua/landerwells/colorscheme.lua
+++ lua/landerwells/colorscheme.lua
@@ -5,8 +5,15 @@ require("boo-colorscheme").use({
   theme = "boo"
 })
 vim.cmd("colorscheme oxocarbon")
-vim.cmd("highlight GitSignsAdd guifg=#58d354")
-vim.cmd("highlight GitSignsChange guifg=#ffae44")
+vim.cmd("highlight RainbowDelimiterRed guifg=#ffd700")
+vim.cmd("highlight RainbowDelimiterYellow guifg=#da70d6")
+vim.cmd("highlight RainbowDelimiterBlue guifg=#179fff")
+vim.cmd("highlight RainbowDelimiterOrange guifg=#ffd700")
+vim.cmd("highlight RainbowDelimiterGreen guifg=#da70d6")
+vim.cmd("highlight RainbowDelimiterViolet guifg=#179fff")
+vim.cmd("highlight Normal guibg=#1d1d1d")
+-- vim.cmd("highlight GitSignsAdd guifg=#58d354")
+-- vim.cmd("highlight GitSignsChange guifg=#ffae44")
 -- end themery block
 
 -- alternate method for transparency
