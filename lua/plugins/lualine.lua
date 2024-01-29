57c582d800beedaab57f74ae89fbbba875b57cd5
landerwells <landerwells@gmail.com>
2024-01-28 22:33:55 -0500

    Updated Lualine colors

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "purple",
-			},
-		})
-	end,
+  -- lualine      https://github.com/nvim-lualine/lualine.nvim
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        disabled_filetypes = { "alpha", "dashboard", "neo-tree" },
+        -- component_separators = { left = '', right = '' },
+        component_separators = { left = "|", right = "|" },
+        -- section_separators = { left = '', right = '' },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_x = { "encoding", { "fileformat", symbols = { unix = "" } }, "filetype" },
+      },
+    })
+  end,
 }
