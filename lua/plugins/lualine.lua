2976e4dd905190454421c938c1777717a33548d7
landerwells <landerwells@gmail.com>
2024-03-06 18:51:09 -0600

    updated lualine, and adding a breakpoint for removing neo-tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,33 +1,69 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
-    -- Custom component to display Harpoon mark status
-    local harpoon_mark_indicator = function()
-      local marks = require('harpoon.mark').get_current_marks()
-      if next(marks) ~= nil then
-        -- Check if the current file is marked. Adjust the indicator as needed.
-        return '🔖'
-      else
-        return ''
-      end
-    end
 
-    require("lualine").setup({
-      options = {
-        theme = "auto",
-        disabled_filetypes = { "alpha", "dashboard", "neo-tree" },
-        component_separators = { left = "|", right = "|" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_x = {
-          "encoding",
-          { "fileformat", symbols = { unix = "" } },
-          "filetype",
-          harpoon_mark_indicator, -- Add the custom component here
-        },
-      },
-    })
-  end,
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
+require('lualine').setup {
+  options = {
+    theme = "auto",
+    disabled_filetypes = { "neo-tree", "" },
+    component_separators = '',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = {
+      '%=', --[[ add your center compoentnts here in place of this comment ]]
+    },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}  end,
 }
 
