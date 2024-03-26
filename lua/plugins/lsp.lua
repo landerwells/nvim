675b773593ed24ce66877f804f4fff980880f8dc
landerwells <landerwells@gmail.com>
2024-03-26 12:09:52 -0400

    added haskell support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,6 +25,9 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
+      lspconfig.hls.setup({
+        capabilities = capabilities,
+      })
       lspconfig.pyright.setup({
         capabilities = capabilities,
         filetypes = {"python"},
